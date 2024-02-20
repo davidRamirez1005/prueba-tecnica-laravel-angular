896db53700a606fbf21efa386b1871c85bbffc58
davidRamirez1005 <ramirezjd1005@gmail.com>
2024-02-20 16:06:45 -0500

    test: :test_tube:

--- src/app/pages/detalle-persona/detalle-persona.component.spec.ts
+++ src/app/pages/detalle-persona/detalle-persona.component.spec.ts
@@ -1,23 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DetallePersonaComponent } from './detalle-persona.component';
+import { PersonaDetalleService } from '../../domains/shared/services/persona-detalle.service';
+import { of } from 'rxjs';
 
 describe('DetallePersonaComponent', () => {
   let component: DetallePersonaComponent;
   let fixture: ComponentFixture<DetallePersonaComponent>;
+  let mockPersonaDetalleService: jasmine.SpyObj<PersonaDetalleService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DetallePersonaComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    mockPersonaDetalleService = jasmine.createSpyObj('PersonaDetalleService', ['getPersonas']);
+
+    TestBed.configureTestingModule({
+      declarations: [DetallePersonaComponent],
+      providers: [
+        { provide: PersonaDetalleService, useValue: mockPersonaDetalleService }
+      ]
+    });
+
     fixture = TestBed.createComponent(DetallePersonaComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load personas on valid search', () => {
+    const personasMock = [
+      { id: 1, nombre: 'name', apellido: 'lastname', email: 'namee@example.com', telefono: '123456789', cedula: 1234567890 },
+      { id: 2, nombre: 'namedos', apellido: 'lastname2', email: 'namedos@example.com', telefono: '987654321', cedula: 987654321 }
+    ];
+    const cedulaMock = '1234567890';
+
+    component.searchPerson.setValue(cedulaMock);
+    mockPersonaDetalleService.getPersonas.and.returnValue(of(personasMock));
+
+    component.changeHandler();
+
+    expect(component.loading).toBe(false);
+    expect(component.personas).toEqual(personasMock);
+  });
+
+  it('should handle error on invalid search', () => {
+    spyOn(console, 'log');
+
+    component.changeHandler();
+
+    expect(component.loading).toBe(false);
+    expect(console.log).toHaveBeenCalledWith('El formulario no es válido.');
+    expect(console.log).toHaveBeenCalledWith('Errores:', component.searchPerson.errors);
   });
 });
--- src/app/pages/lista-personas/lista-personas.component.spec.ts
+++ src/app/pages/lista-personas/lista-personas.component.spec.ts
@@ -1,23 +1,57 @@
+import { Persona } from './../../domains/shared/models/persona.model';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListaPersonasComponent } from './lista-personas.component';
+import { PersonaService } from '../../domains/shared/services/persona.service';
+import { of } from 'rxjs';
 
 describe('ListaPersonasComponent', () => {
   let component: ListaPersonasComponent;
   let fixture: ComponentFixture<ListaPersonasComponent>;
+  let mockPersonaService: jasmine.SpyObj<PersonaService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ListaPersonasComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    mockPersonaService = jasmine.createSpyObj('PersonaService', ['getPersonas']);
+
+    TestBed.configureTestingModule({
+      declarations: [ListaPersonasComponent],
+      providers: [
+        { provide: PersonaService, useValue: mockPersonaService }
+      ]
+    });
+
     fixture = TestBed.createComponent(ListaPersonasComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize personas and loading', () => {
+    const personasMock: Persona[] = [
+      { id: 1, nombre: 'name', apellido: 'lastname', email: 'namee@example.com', telefono: '123456789', cedula: 1234567890 },
+      { id: 2, nombre: 'namedos', apellido: 'lastname2', email: 'namedos@example.com', telefono: '987654321', cedula: 987654321 }
+    ];
+
+    mockPersonaService.getPersonas.and.returnValue(of(personasMock));
+
+    component.ngOnInit();
+
+    expect(component.loading).toBe(false);
+    expect(component.personas).toEqual(personasMock);
+  });
+
+
+
+  it('should handle error during initialization', () => {
+    const errorMock = 'Error fetching personas';
+    mockPersonaService.getPersonas.and.throwError(errorMock);
+
+    spyOn(console, 'error');
+
+    component.ngOnInit();
+
+    expect(component.loading).toBe(false);
+    expect(console.error).toHaveBeenCalledWith('Error fetching personas', errorMock);
   });
 });
